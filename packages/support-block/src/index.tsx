9fcb53d1def8df45fea07bb6a93885245fdce919
huangbo <qqjay2013@gmail.com>
2022-06-26 17:41:32 +0800

    feat: 注释测试代码111

--- packages/support-block/src/index.tsx
+++ packages/support-block/src/index.tsx
@@ -107,27 +107,27 @@ export class CreateSupportBlock {
     // }
   }
 }
-const env = process.env.NODE_ENV || "production";
-console.log(env, "env");
-// // console.log(env, "env");
-if (env == "development") {
-  const helpBlock = new CreateSupportBlock({
-    beforeRender() {
-      console.log("beforeRender");
-    },
-    afterRender() {
-      console.log("afterRedner");
-    },
-    httpError(code, data) {
-      // 401
-      // { code: 401 ,message: "暂未登录或token已经过期"}
-      console.log(code, data);
-    }, //   return Promise.resolve("123"); //   console.log(url, { method, data }); //   console.log("123"); // request(url, { method, data }) {
-    // },
-    // systemId: "167096554103328853",
-    // systemId: "167082792923254878",
-    systemId: "81869552404688911",
-    initMenuCode: "CONTRACT",
-    baseURL: "http://ymsl.kxgcc.com:30872",
-  });
-}
+// const env = process.env.NODE_ENV || "production";
+// console.log(env, "env");
+// // // console.log(env, "env");
+// if (env == "development") {
+//   const helpBlock = new CreateSupportBlock({
+//     beforeRender() {
+//       console.log("beforeRender");
+//     },
+//     afterRender() {
+//       console.log("afterRedner");
+//     },
+//     httpError(code, data) {
+//       // 401
+//       // { code: 401 ,message: "暂未登录或token已经过期"}
+//       console.log(code, data);
+//     }, //   return Promise.resolve("123"); //   console.log(url, { method, data }); //   console.log("123"); // request(url, { method, data }) {
+//     // },
+//     // systemId: "167096554103328853",
+//     // systemId: "167082792923254878",
+//     systemId: "81869552404688911",
+//     initMenuCode: "CONTRACT",
+//     baseURL: "http://ymsl.kxgcc.com:30872",
+//   });
+// }
