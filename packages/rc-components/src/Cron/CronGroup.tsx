35bc1dba3c09cb1ed593962b6b61311b6df770dc
huangbo <qqjay2013@gmail.com>
2022-08-01 10:39:06 +0800

    feat: 年日计算

--- packages/rc-components/src/Cron/CronGroup.tsx
+++ packages/rc-components/src/Cron/CronGroup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IDayNumberInputProps, CronDayNumberInput } from './DayNumberInput';
-import { Space } from 'antd';
+import { Space, SpaceProps } from 'antd';
 import { IDaySelectProps, CronDaySelect } from './DaySelect';
 import { IMonSelectProps, CronMonSelect } from './MonSelect';
 import styled from 'styled-components';
@@ -9,29 +9,25 @@ export type ICronGroupBaseItem = string | React.ReactElement;
 
 export interface ICronGroupBaseProps {
   items: ICronGroupBaseItem[];
+  spaceProps?: SpaceProps;
 }
 
 const INTERNAL_PREFIX_KEY = 'cron-group-key';
 
 const CronGroupSpan = styled.span`
+  font-size: 14px;
 
+  font-weight: 400;
+  color: rgba(0, 0, 0, 0.85);
+  line-height: 20px;
+`;
 
-font-size: 14px;
-
-font-weight: 400;
-color: rgba(0,0,0,0.85);
-line-height: 20px;
-
-
-
-`
-
-export function CronGroupBase({ items }: ICronGroupBaseProps) {
+export function CronGroupBase({ items, spaceProps = {} }: ICronGroupBaseProps) {
   if (!items || !Array.isArray(items)) {
     return null;
   }
   return (
-    <Space>
+    <Space wrap={true} {...spaceProps}>
       {items.map((item, index) => {
         if (typeof item === 'string') {
           return <CronGroupSpan key={`${INTERNAL_PREFIX_KEY}-${index}`}>{item}</CronGroupSpan>;
